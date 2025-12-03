46515173bd7d5392b7b1197db70f2005e69d83a4
saparaliyevshaxzodbek-dot <saparaliyevshaxzodbek@gmail.com>
2025-12-03 13:56:20 +0500

    dars2

---
+++ Dars2/2-darsuygavaziyfasi/2-darsuygavaziyfasi/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2_darsuygavaziyfasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intvalu;
+            Console.WriteLine("uch honali son kiriting");
+            intvalu = int.Parse(Console.ReadLine());
+
+            int bir = intvalu % 10;
+            int on = intvalu % 100;
+            int on1 = (int) Math.Floor(on / 10.0);
+            int yuz = (int)Math.Floor(intvalu / 100.0);
+
+            Console.WriteLine(bir+ "" + on1 + "" + yuz);
+        }
+    }
+}
