e4e4af16e0177822dcb8f87c0057d92c214b4df5
saparaliyevshaxzodbek-dot <saparaliyevshaxzodbek@gmail.com>
2025-12-03 14:14:11 +0500

    2dars

--- Dars2/2-darsuygavaziyfasi/2-darsuygavaziyfasi/Program.cs
+++ Dars2/2-darsuygavaziyfasi/2-darsuygavaziyfasi/Program.cs
@@ -4,16 +4,32 @@
     {
         static void Main(string[] args)
         {
-            int intvalu;
-            Console.WriteLine("uch honali son kiriting");
-            intvalu = int.Parse(Console.ReadLine());
+            //1-masala
+            //int intvalu;
+            //Console.WriteLine("uch honali son kiriting");
+            //intvalu = int.Parse(Console.ReadLine());
 
-            int bir = intvalu % 10;
-            int on = intvalu % 100;
-            int on1 = (int) Math.Floor(on / 10.0);
-            int yuz = (int)Math.Floor(intvalu / 100.0);
+            //int bir = intvalu % 10;
+            //int on = intvalu % 100;
+            //int on1 = (int) Math.Floor(on / 10.0);
+            //int yuz = (int)Math.Floor(intvalu / 100.0);
 
-            Console.WriteLine(bir+ "" + on1 + "" + yuz);
+            //Console.WriteLine(bir+ "" + on1 + "" + yuz);
+
+            //2-masala
+            int a = 4;
+            int b = 3;
+            int c = 1;
+            int d = 5;
+
+            if(a > c && b > c && d > c && c % 2 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
         }
     }
 }
