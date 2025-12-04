e0371b7d237f180f7fdcf03c2f1d3a667c22e8cc
saparaliyevshaxzodbek-dot <saparaliyevshaxzodbek@gmail.com>
2025-12-04 15:10:12 +0500

    uyvazifa

--- Dars2/2-darsuygavaziyfasi/2-darsuygavaziyfasi/Program.cs
+++ Dars2/2-darsuygavaziyfasi/2-darsuygavaziyfasi/Program.cs
@@ -31,6 +31,192 @@
             //    Console.WriteLine(false);
             //}
 
+            //1-misol
+            //int intvalu = 2;
+            //intvalu = intvalu * 60;
+            //Console.Write( "soatda daqiqaga otildi:");
+            //Console.WriteLine(intvalu);
+
+            //2-misol
+            //int intvalu = 5;
+            //intvalu = intvalu * 24;
+            //Console.WriteLine(intvalu);
+
+            //3-misol
+            //int intvalu = 4;
+            //intvalu = intvalu * 60;
+            //Console.Write("sekund:");
+            //Console.WriteLine(intvalu);
+
+            //4-misol
+            //int intvalu = 2;
+            //intvalu = intvalu + 10;
+            //Console.WriteLine(intvalu);
+
+            //5-misol
+            //int intvalu = 14;
+            //intvalu = intvalu - 10;
+            //Console.WriteLine(intvalu);
+
+            //6-masala
+            //int intvalu = 2;
+            //intvalu = intvalu * 10;
+            //Console.WriteLine(intvalu);
+
+            //7-masala
+            //int intvalu = 2;
+            //intvalu = intvalu * intvalu;
+            //Console.WriteLine(intvalu);
+
+            //8-masala
+            //int intvalu = 2;
+            //intvalu = intvalu * intvalu * intvalu;
+
+            //Console.WriteLine(intvalu);
+
+            //9-masala
+            //int a = 2;
+            //int b = 3;
+            //int intvalue = a + b;
+            //Console.WriteLine(intvalue);
+
+            //10-masala
+            //int a = 2;
+            //int b = 3;
+            //int intvalue = a * b;
+            //Console.WriteLine(intvalue);
+
+            //11-masala
+            //int a = 1;
+            //int b = 2;
+            //int c = 3;
+            //int d = 4;
+
+            //int intvalu = a + b + c + d;
+            //Console.WriteLine(intvalu);
+
+            //12/-masala
+            //int a = 2;
+            //int b = 3;
+            //int c = 4;
+
+            //int intvalu = (a + b + c) / 3;
+            //Console.WriteLine(intvalu);
+
+            //13-misol
+            //int a = 3;
+            //int katta = a + 1;
+            //int kichik = a - 1;
+
+            //Console.WriteLine(katta);
+            //Console.WriteLine(kichik);
+
+            //14-misol
+            //int a = 4;
+            //int b = 2;
+
+            //int qoshish = a + b;
+            //int ayirish = a - b;
+            //int kopaytma = a * b;
+            //int boluv = a / b;
+
+            //Console.WriteLine(qoshish +" " + ayirish +" "+ kopaytma + " " + boluv);
+
+            //15-masala
+            //int a = 12;
+
+            //int intvalu = 12 % 10;
+            //Console.WriteLine(intvalu);
+
+            //16-masala\
+            //int a = 123;
+
+            //int onlar = a % 100;
+            //int intvalue = (int)Math.Floor(onlar / 10.0);
+
+            //Console.WriteLine(intvalue);
+
+            //17-masala
+            //int a = 1234;
+            //int bir = (int)Math.Floor(a / 1000.0);
+            //int son = a % 1000;
+            //int ikki = (int)Math.Floor(son / 100.0);
+            //int son2 = son % 100;
+            //int uch = (int)Math.Floor(son2 / 10.0);
+            //int tort = son2 % 10;
+
+            //Console.Write(bir + "" + ikki + "" + tort + "" + uch);
+
+            //18-misol
+            //int son = 2;
+
+            //int kvadrat = son * son;
+            //int natija = kvadrat * kvadrat;
+
+            //Console.WriteLine($"{son}ni 4 - darajasi {natija} ");
+
+            //19-masala
+
+            //int dollir = 2;
+            //int evro = 3;
+
+            //int som = dollir * 12600;
+            //int som1 = evro * 14500;
+
+            //int natija = som + som1;
+
+            //Console.WriteLine(natija  + "som" );
+
+            //20-misol
+
+            //int son = 7;
+
+            //if(son % 2 == 0)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+
+            //21-misol
+            //int son = 2;
+
+            //if (son % 2 != 0)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            //22-misol
+            //int son = 10;
+
+            //if (son >= 10 && son <= 99)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            //23-misol
+
+            //int son1 = 21;
+            //int son2 = 24;
+
+            //if (son1 % 2 == 0 && son2 % 2 == 0)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
 
         }
     }
