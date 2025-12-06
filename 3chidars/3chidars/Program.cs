7903990e5cd1e3902dbfbbd42a0380773d166fd6
saparaliyevshaxzodbek-dot <saparaliyevshaxzodbek@gmail.com>
2025-12-06 13:28:00 +0500

    uygavaziyfakodlari

---
+++ 3chidars/3chidars/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _3chidars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //for (int i = 100; i < 999; i++)
+        //{
+        //    int bir = i % 10;
+        //    int on1 = i % 100;
+        //    int on = (int)Math.Floor(on1 / 10.0);
+        //    int yuz = (int)Math.Floor(i / 100.0);
+
+        //    if(on % 2 != 0 && bir % 2 != 0 && yuz % 2 != 0)
+        //    {
+        //        Console.Write($"{i}, ");
+        //    }
+        //}
+
+        int a = 3;
+        int b = 5;
+
+        int sana1 = 0;
+        int sana2 = 0;
+
+
+        for(int i = 0; i <= a; i++)
+        {
+            if(a % i == 0)
+            {
+                sana1++;
+            }
+        }
+
+        for (int i = 0; i <= b; i++)
+        {
+            if (b % i == 0)
+            {
+                sana2++;
+            }
+        }
+
+        if (sana1 == 0 && sana2 == 2)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+        else
+        {
+           int sum1 = a * b;
+            Console.WriteLine(sum1);
+        }
+
+    }
+}
---
+++ 3darsuygavaziyfasi/3darsuygavaziyfasi/Program.cs
@@ -0,0 +1,343 @@
+﻿namespace _3darsuygavaziyfasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-misol
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    Console.Write($"{i}, ");
+            //}
+
+            //2-misol
+
+            //for (int i = 15; i > 2; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //3-misol
+            //for(int i = 80; i > 20; i--)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //4-misol
+            //Console.WriteLine("Birinchi sonni kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Ikkinchi sonni kiriting");
+            //int b = int.Parse(Console.ReadLine());
+
+            //if(a < b)
+            //{
+            //    for(int i = a; i < b; i++)
+            //    {
+            //        if(i % 5 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("hato son kiritdingiz");
+            //}
+
+            //5-misol
+
+            //int yig = 0;
+
+            //    for(int i = 1; i < 100; i++)
+            //{
+            //    if(i % 2 != 0)
+            //    {
+            //        yig = yig + i;
+            //    }
+            //}
+            //Console.WriteLine(yig);
+
+            //6-misol
+
+
+            // for (int i = 100; i <= 999; i++)
+            //{
+            //    int bir = i / 1 % 10;
+            //    int on = i % 100 / 10;
+            //    int yuz = i / 100;
+
+            //    int natija = bir + on + yuz;
+
+            //    if( natija > 20)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //7-misol
+
+            //for(int i = 100; i <= 999; i++)
+            //{
+            //    int on = i % 100 / 10;
+
+            //    if (on == 2 || on == 5)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //8-misol
+            //Console.Write("Birinchi sonnni kiriting:");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("Ikkinchi sonnni kiriting:");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for(int i = 1; i <= b; i++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //9-misol
+            //Console.WriteLine("Son kiriting");
+            //int son = int.Parse(Console.ReadLine());
+
+            //int yig = 0;
+
+            //for(int i = 1; i <= son; i++)
+            //{
+            //    if(son % i == 0)
+            //    {
+            //        yig = yig + i;
+            //    }
+            //}
+            //Console.WriteLine(yig);
+
+            //10-misol
+
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //int yig = 0;
+
+            //for(int i = 1; i <= son; i++)
+            //{
+            //    if(son % i == 0)
+            //    {
+            //        yig++;
+            //    }
+            //}
+
+            //if(yig == 2)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            //12-misol
+
+            //Console.Write("23 dan katta son kiriting:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int soni = 0;
+
+            //for(int i = 23; i < n; i++)
+            //{
+            //    if(i % 3 != 0)
+            //    {
+            //        Console.WriteLine($"{i} Uchga bolinmaydigan sonl");
+            //        soni++;
+            //    }
+            //}
+
+            //Console.WriteLine($"{soni} uchga bolinmaydigan sonlarni soni");
+
+            //13-misol
+            //int n = 2;
+            //int k = 3;
+
+            //int natija = 1;
+
+            //for(int i = 1; i <= k; i++)
+            //{
+            //    natija = natija * n;
+            //}
+            //Console.WriteLine(natija);
+
+
+            //14-misol
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //string text = $"{son}";
+            //int sum = 0;
+
+            //for(int i = 0; i < text.Length; i++)
+            //{
+            //    sum++;
+            //}
+
+            //Console.WriteLine($"kiritilgan son: {sum} honali");
+
+            //15-misol
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //string text = $"{son}";
+            //int sum = 0;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    sum = sum + text[i] - '0';
+            //}
+            //Console.WriteLine(sum);
+
+            //16-misol
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //string text = $"{son}";
+            //int sum = 0;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    int raqam = text[i] - '0';
+            //    if (raqam % 2 == 0)
+            //    {
+            //        sum++;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //17-misol
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //string text = $"{son}";
+            //int sum = 0;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    int raqam = text[i] - '0';
+            //    if (raqam % 2 != 0)
+            //    {
+            //        sum++;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //18-misol
+            //Console.Write("Son kiriting:");
+            //int son = int.Parse(Console.ReadLine());
+
+            //string text = $"{son}";
+            //int sum = 0;
+
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    int raqam = text[i] - '0';
+            //    if (raqam == 5)
+            //    {
+            //        sum++;
+            //    }
+
+            //}
+
+            //if(sum > 0)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            //19-misol
+            //for(int i = 2; i <= 9; i++)
+            //{
+            //    int sum = 1;
+            //    for(int j = 1; j <= 10; j++)
+            //    {
+            //        sum = i * j;
+            //        Console.Write($"{i} x {j} = {sum} ," );
+            //    }
+            //}
+
+            //20-misol
+
+            //int a = 2;
+            //int b = 6;
+
+            //for(int i = a + 1; i < b; i++)
+            //{
+            //    for(int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //}
+
+            //21-misol
+            //for(int i = 1; i < 100; i++)
+            //{
+            //    int sum = 0;
+            //    for(int j = 1; j < 100;  j++)
+            //    {
+            //        if(i % j == 0)
+            //        {
+            //            sum++;
+
+            //        }
+
+            //    }
+            //    if(sum == 2)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //22-misol
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    string qator = "";
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        qator = qator + "*";
+            //    }
+            //    Console.WriteLine(qator);
+            //}
+
+            //23-misol
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string qator = "";
+                for (int j = 1; j <= i; j++)
+                {
+                    if(i == j)
+                    {
+                        qator = qator + "*";
+                    } else
+                    {
+                        qator = qator + " ";
+                    }
+                }
+                Console.WriteLine(qator);
+            }
+
+
+        }
+    }
+}
